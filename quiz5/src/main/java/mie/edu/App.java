7cb053eda096548513194c6b8e06c39d43260280
NaguaG <naranguag@gmail.com>
2023-10-11 23:07:14 -0500

    added demo

---
+++ quiz5/src/main/java/mie/edu/App.java
@@ -0,0 +1,15 @@
+package mie.edu;
+
+import com.sun.tools.javac.main.CommandLine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
